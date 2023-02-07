6a8807c0410777994ee8763a0f7ae7ea664dfd17
YC815 <minnecraft.YC815@gmail.com>
2023-02-07 10:32:22 +0800

    不能用

--- note-web/pages/index.js
+++ note-web/pages/index.js
@@ -1,33 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const HomePage = () => {
-const [textAreas, setTextAreas] = useState([
-{ id: 1 }
-]);
+  const [textAreas, setTextAreas] = useState([
+    { id: 1, content: "" }
+  ]);
 
-const addTextArea = () => {
-setTextAreas([...textAreas, { id: textAreas.length + 1 }]);
-};
+  useEffect(() => {
+    axios.get("/data/note.json").then(res => {
+      setTextAreas(res.data.note);
+    });
+  }, []);
 
-const removeTextArea = (id) => {
-setTextAreas(textAreas.filter(t => t.id !== id));
-};
+  const addTextArea = () => {
+    setTextAreas([...textAreas, { id: textAreas.length + 1, content: "" }]);
+  };
 
-return (
-<div style={{ backgroundColor: '#333', height: '100vh' }}>
-{textAreas.map(t => (
-<div key={t.id} style={{ display: 'flex', alignItems: 'flex-end' }}>
-<button style={{ backgroundColor: 'red', color: 'white', padding: '5px 10px', position: 'relative', bottom: '0' }} onClick={() => removeTextArea(t.id)}>
-╳
-</button>
-<textarea style={{ padding: '20px', width: '332px', height: '234px', float: 'left', marginRight: '10px' }} />
-</div>
-))}
-<button style={{ backgroundColor: 'green', color: 'white', padding: '10px 20px', position: 'absolute', bottom: '0', right: '0' }} onClick={addTextArea}>
-+
-</button>
-</div>
-);
+  const removeTextArea = (id) => {
+    setTextAreas(textAreas.filter(t => t.id !== id));
+  };
+
+  const handleChange = (e, id) => {
+    const updatedTextAreas = textAreas.map(t => {
+      if (t.id === id) {
+        return { ...t, content: e.target.value };
+      }
+      return t;
+    });
+    setTextAreas(updatedTextAreas);
+  };
+
+  useEffect(() => {
+    axios.post("/data/note.json", { note: textAreas });
+  }, [textAreas]);
+
+  return (
+    <div style={{ backgroundColor: '#333', height: '100vh' }}>
+      {textAreas.map(t => (
+        <div key={t.id} style={{ display: 'flex', alignItems: 'flex-start' }}>
+          <button style={{ backgroundColor: 'red', color: 'white', padding: '5px 10px' }} onClick={() => removeTextArea(t.id)}>
+            ╳
+          </button>
+          <textarea 
+            style={{ padding: '20px', width: '2500px', height: '234px' }} 
+            value={t.content}
+            onChange={e => handleChange(e, t.id)}
+          />
+        </div>
+      ))}
+      <button style={{ backgroundColor: 'green', color: 'white', padding: '10px 20px', position: 'fixed', bottom: '20px', right: '20px' }} onClick={addTextArea}>
+        +
+      </button>
+    </div>
+  );
 };
 
 export default HomePage;
