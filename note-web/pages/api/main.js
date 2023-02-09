2b20b60c6e751c6c1fa4c42d04dad5851a848f94
YC815 <yushun815@gmail.com>
2023-02-09 20:30:01 +0800

    1.0發布！

--- note-web/pages/api/main.js
+++ note-web/pages/api/main.js
@@ -1,12 +1,29 @@
-import db from "../../data/note.json"
-
+/* import axios from "axios"; */
+import db from "../../data/note.json";
+import fs from "fs";
 
 export default function handler(req, res) {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const data = req.body;
-    // Do something with the data, e.g. save it to a database, process it, etc.
-    res.status(200).json({ message: 'Data received', data });
-  } else if (req.method === 'GET') {
+    // console.log(data);
+    fs.writeFileSync("data/note.json", JSON.stringify(data));
+    console.log("post data:", data);
+    res.status(200).json({ message: "Data received", data });
+  } else if (req.method === "GET") {
+    const d = fs.readFileSync("data/note.json", "utf-8");
+    console.log("getd:", d);
     res.status(200).json(db);
   }
 }
+
+/* fs.readFileSync("data/note.json", "utf-8", (err, data) => { */
+/* onsole.log("data:", data); */
+/*   // if (err) { */
+/*   //   console.error(err); */
+/*   //   res.status(500).json({ error: "Error reading file" }); */
+/*   // } else { */
+/*   const db2 = JSON.parse(data); */
+/*   console.log("db2:", db2); */
+/*   res.status(200).json(db2); */
+/*   // } */
+/* }); */
--- note-web/pages/index.js
+++ note-web/pages/index.js
@@ -1,53 +1,83 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 // import db from '../data/note.json'
 
 const HomePage = () => {
-  const [textAreas, setTextAreas] = useState([
-    { id: 1, content: "" }
-  ]);
+  const [textAreas, setTextAreas] = useState([{ id: 1, content: "" }]);
 
   // console.log(db.note)
 
   useEffect(() => {
-    
-    axios.get("/api/main").then(res => {
+    axios.get("/api/main").then((res) => {
       setTextAreas(res.data.note);
     });
   }, []);
 
   const addTextArea = () => {
     setTextAreas([...textAreas, { id: textAreas.length + 1, content: "" }]);
+    axios
+      .post("/api/main", { note: textAreas })
+      .then((res) => console.log(res))
+      .catch((err) => console.error(err));
   };
 
   const removeTextArea = (id) => {
-    setTextAreas(textAreas.filter(t => t.id !== id));
+    setTextAreas(textAreas.filter((t) => t.id !== id));
   };
 
-  const textAreaChangeHandler = (e) => {
-    
-    axios.post("/api/main", { note: textAreas });
-  }
+  const handleTextAreaChange = (event, id) => {
+    console.log("id:", id);
+    const newTextAreas = textAreas.map((textArea) =>
+      textArea.id === id
+        ? { ...textArea, content: event.target.value }
+        : textArea
+    );
+    setTextAreas(newTextAreas);
+    axios
+      .post("/api/main", { note: newTextAreas })
+      .then((res) => console.log("post success:", res))
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    console.log("ta:", textAreas);
+  }, [textAreas]);
 
   return (
-    <div style={{ backgroundColor: '#333', height: '100vh' }}>
-      {textAreas.map(t => (
-        <div key={t.id} style={{ display: 'flex', alignItems: 'flex-start' }}>
-          <button style={{ backgroundColor: 'red', color: 'white', padding: '5px 10px' }} onClick={() => removeTextArea(t.id)}>
+    <div style={{ backgroundColor: "#333", height: "100vh" }}>
+      {textAreas.map((t) => (
+        <div key={t.id} style={{ display: "flex", alignItems: "flex-start" }}>
+          <button
+            style={{
+              backgroundColor: "red",
+              color: "white",
+              padding: "5px 10px",
+            }}
+            onClick={() => removeTextArea(t.id)}
+          >
             ╳
           </button>
-          <textarea 
-            style={{ padding: '20px', width: '2500px', height: '234px' }} 
+          <textarea
+            style={{ padding: "20px", width: "2500px", height: "234px" }}
             value={t.content}
-            onChange={ textAreaChangeHandler }
+            onChange={(event) => handleTextAreaChange(event, t.id)}
           />
         </div>
       ))}
-      <button style={{ backgroundColor: 'green', color: 'white', padding: '10px 20px', position: 'fixed', bottom: '20px', right: '20px' }} onClick={addTextArea}>
+      <button
+        style={{
+          backgroundColor: "green",
+          color: "white",
+          padding: "10px 20px",
+          position: "fixed",
+          bottom: "30px",
+          right: "20px",
+        }}
+        onClick={addTextArea}
+      >
         +
       </button>
     </div>
   );
 };
-
 export default HomePage;
